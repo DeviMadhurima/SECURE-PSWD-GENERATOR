5fe0407d18781afdfa24b72017c7a4a984de1119
DeviMadhurima <devimadhurimachalapakame27@gmail.com>
2025-12-06 20:00:04 +0530

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,39 @@
+// Name of the cache
+const CACHE_NAME = "passgen-cache-v1";
+
+// Files to cache
+const ASSETS = [
+  "./",
+  "./index.html",
+  "./manifest.json",
+  "./icon.png"
+];
+
+// Install event: cache files
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS);
+    })
+  );
+});
+
+// Fetch event: serve cached files if offline
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
+
+// Activate event: clean old caches
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(
+        keys.filter((key) => key !== CACHE_NAME).map((key) => caches.delete(key))
+      )
+    )
+  );
+});
